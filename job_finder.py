f365539d843b10abfd9f9f6cc670b41f1ed55f5c
keelan <pkarmstr@gmail.com>
2014-04-15 23:10:15 -0400

    Initial commit

---
+++ job_finder.py
@@ -0,0 +1,42 @@
+__author__ = 'keelan'
+
+import time
+import praw
+import sys
+import json
+from os.path import join
+
+basedir = sys.argv[1]
+
+submission  = "http://www.reddit.com/r/AskReddit/comments/1zwthi/whats_your_job_how_much_do_you_make_per_year_how/"
+user_agent = "scraping job info from reddit for science"
+
+r = praw.Reddit(user_agent)
+
+jobs_submission = r.get_submission(submission)
+
+all_comments = jobs_submission.comments
+all_comments_body = {}
+more_comments = True
+file_i = 0
+num_comments = 0
+
+start = time.time()
+while more_comments:
+    more_comments = False
+    for c in all_comments:
+        try:
+            all_comments_body[c.link_id] = c.body
+            num_comments += 1
+        except AttributeError, e:
+            all_comments = c.comments()
+            file_i += 1
+            outfile = join(basedir, "jobs-{:d}.json".format(file_i))
+            with open(outfile, "w") as f_out:
+                f_out.write(json.dumps(all_comments_body))
+            more_comments = True
+
+with open("jobs_scraper.log", "w") as f_out:
+    f_out.write("wrote to {:d} files\n".format(file_i))
+    f_out.write("saved {:d} comments\n".format(num_comments))
+    f_out.write("time took scraping was {:f} seconds\n".format(time.time()-start))
