0b2fd85b7a2966fd6755e1aee731b2992879054d
keelan <pkarmstr@gmail.com>
2014-04-16 00:18:00 -0400

    implemented saving 1000 comments at a time

--- job_finder.py
+++ job_finder.py
@@ -18,6 +18,8 @@ basedir = sys.argv[1]
 submission  = "http://www.reddit.com/r/AskReddit/comments/1zwthi/whats_your_job_how_much_do_you_make_per_year_how/"
 user_agent = "scraping job info from reddit for science"
 
+start = time.time()
+
 r = praw.Reddit(user_agent)
 
 jobs_submission = r.get_submission(submission)
@@ -26,24 +28,35 @@ all_comments = jobs_submission.comments
 all_comments_body = {}
 more_comments = True
 file_i = 0
-num_comments = 0
+num_comments = 1
 
-start = time.time()
 while more_comments:
     more_comments = False
     for c in all_comments:
         try:
             all_comments_body[c.id] = c.body
             num_comments += 1
+
         except AttributeError, e:
-            #we've hit a MoreComments object, no `body` attribute
+            # we've hit a MoreComments object, no `body` attribute
             all_comments = c.comments()
+            more_comments = True
+            all_comments_body = {}
+
+        if num_comments%1000 == 0:
+            # save every 1000 comments into a separate file
             file_i += 1
             outfile = join(basedir, "jobs-{:d}.json".format(file_i))
             with open(outfile, "w") as f_out:
                 f_out.write(json.dumps(all_comments_body))
-            more_comments = True
-            all_comments_body = {}
+
+    if all_comments_body:
+        # save anything to disk we have left over
+        file_i += 1
+        outfile = join(basedir, "jobs-{:d}.json".format(file_i))
+        with open(outfile, "w") as f_out:
+            f_out.write(json.dumps(all_comments_body))
+
 
 with open("jobs_scraper.log", "w") as f_out:
     f_out.write("wrote to {:d} files\n".format(file_i))
