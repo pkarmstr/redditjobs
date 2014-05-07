671b6fd1b64483f4bc4a9c5a7e4feba56287d96c
keelan <pkarmstr@gmail.com>
2014-05-07 04:49:53 -0400

    fixed mutual bootstrapping

--- bootstrap_pipeline.py
+++ bootstrap_pipeline.py
@@ -25,20 +25,25 @@ class MutualBootStrapper:
             self.find_patterns = self.find_patterns_tagged
             self.find_seeds = self.find_seeds_tagged
         self.seeds = set(seeds)
+        self.best_extraction_patterns = set()
         self.pattern_alphabet = Alphabet()
         if patterns is not None:
             for p in patterns:
                 self.pattern_alphabet.add(p)
         self.candidate_seeds = defaultdict(set)
+        self.n_counter_sets = None # import for getting candidate seeds
+        self.f_counter_sets = None
         self.n_pattern_array = None
         self.f_pattern_array = None
         self.first_pattern_words = set()
 
     def tokenize(self, text):
+        print "tokenizing...",
         all_entries = []
         for entry in text:
             tokenized_entry = self._nested_tokenize(entry)
             all_entries.append(tokenized_entry)
+        print "[DONE]"
         return all_entries
 
     def _nested_tokenize(self, untokenized_sentences):
@@ -56,12 +61,15 @@ class MutualBootStrapper:
                     #mutating the list
 
     def pos_tag(self, tokenized_data):
+        print "POS tagging... ",
         pos_tagged_data = []
         for entry in tokenized_data:
             new_entry = []
             for sentence in entry:
-                new_entry.append(nltk.pos_tag(sentence))
+                tagged = [("__START__", "__START__")].extend(nltk.pos_tag(sentence))
+                new_entry.append(tagged)
             pos_tagged_data.append(new_entry)
+        print "[DONE]"
         return pos_tagged_data
 
     def build_patterns_tagged(self, sentence, index, size):
@@ -70,7 +78,10 @@ class MutualBootStrapper:
         sentence_copy = list(sentence)
         sentence_copy[index] = "<x>",
         while window_start <= index: # this isn't quite right
-            candidate = zip(*sentence_copy[window_start:window_end])[0]
+            try:
+                candidate = zip(*sentence_copy[window_start:window_end])[0]
+            except IndexError:
+                candidate = []
             if len(candidate) > 1:
                 self.pattern_alphabet.add(tuple(candidate))
                 if candidate[0] != "<x>":
@@ -89,8 +100,9 @@ class MutualBootStrapper:
                         self.build_patterns_tagged(sentence, i, 1)
 
     def set_counter_arrays(self):
-        self.n_pattern_array = numpy.ones(self.pattern_alphabet.size())
-        self.f_pattern_array = numpy.ones(self.pattern_alphabet.size())
+        tmp_lst = [[]] * self.pattern_alphabet.size() # must be careful about mutability here
+        self.n_counter_sets = map(set, tmp_lst)
+        self.f_counter_sets = map(set, tmp_lst)
 
     def find_seeds_tagged(self):
         for entry in self.pos_tagged_data:
@@ -117,14 +129,13 @@ class MutualBootStrapper:
                 pattern_index = self.pattern_alphabet.get_index(pattern)
 
                 # increment our counters
-                self.n_pattern_array[pattern_index] += 1
+                self.n_counter_sets[pattern_index].add(candidate_seed)
                 if candidate_seed not in self.seeds:
-                    self.f_pattern_array[pattern_index] += 1
-
-                    # update our candidate seeds
-                    self.candidate_seeds[candidate_seed].add(pattern_index)
+                    self.f_counter_sets[pattern_index].add(candidate_seed)
 
     def calculate_pattern_scores(self):
+        self.n_pattern_array = numpy.array(map(len, self.n_counter_sets))
+        self.f_pattern_array = numpy.array(map(len, self.f_counter_sets))
         self.pattern_scores = (self.f_pattern_array/self.n_pattern_array)*numpy.log2(self.f_pattern_array)
 
     def calculate_seed_scores(self):
@@ -140,24 +151,44 @@ class MutualBootStrapper:
         sorted_candidates = sorted([(v,k) for k,v in self.candidate_seed_scores.iteritems()], reverse=True)
         return zip(*sorted_candidates)[1][:5]
 
-    def clear_data(self):
-        self.candidate_seeds = defaultdict(set)
+    def run__mutual_bootstrapping(self):
+        added_patterns = 0
+        while added_patterns < 10:
+            self.find_patterns()
+            self.set_counter_arrays()
+            self.find_seeds()
+            self.calculate_pattern_scores()
+            best_pattern_index = numpy.nanargmax(self.pattern_scores)
+            while best_pattern_index in self.best_extraction_patterns:
+                self.pattern_scores[best_pattern_index] = -10000000.
+                best_pattern_index = numpy.nanargmax(self.pattern_scores)
+            if self.pattern_scores[best_pattern_index] < 0.7:
+                return False
+            self.best_extraction_patterns.add(best_pattern_index)
+            for seed in self.n_counter_sets[best_pattern_index]:
+                self.candidate_seeds[seed].add(best_pattern_index)
+            added_patterns += 1
+        return True
 
-    def run_iteration(self):
-        self.find_patterns()
-        self.set_counter_arrays()
-        self.find_seeds()
+    def run_meta_bootstrapping(self):
         best_five = self.cull_candidates()
         self.seeds.update(best_five)
-        self.clear_data()
+        self.candidate_seeds = defaultdict(set)
 
     def run(self, num_iterations=50):
         for i in range(num_iterations):
             print "Iteration: {:d}".format(i+1)
-            self.run_iteration()
+            print "running mutual bootstrapping...",
+            keep_going = self.run__mutual_bootstrapping()
+            print "[DONE]"
+            if keep_going:
+                print "running meta bootstrapping...",
+                self.run_meta_bootstrapping()
+                print "[DONE]"
             print "number of seed terms: {:d}".format(len(self.seeds))
             print "number of total patterns: {:d}".format(self.pattern_alphabet.size())
             print "\n"
+
 
     def save_seeds(self, outfile):
         with open(outfile, "w") as f_out:
@@ -166,7 +197,7 @@ class MutualBootStrapper:
     def save_patterns(self, outfile):
         with open(outfile, "w") as f_out:
             patterns = []
-            for pattern_index in range(self.pattern_alphabet.size()):
+            for pattern_index in self.best_extraction_patterns:
                 patterns.append(" ".join(self.pattern_alphabet.get_label(pattern_index)))
             f_out.write("\n".join(s.encode("utf-8") for s in patterns))
 
@@ -180,7 +211,7 @@ if __name__ == "__main__":
     parser.add_argument("--patterns", help="list of patterns to begin with")
 
     all_args = parser.parse_args()
-    all_data = json.load(open(all_args.json_data))
+    all_data = [v for k,v in json.load(open(all_args.json_data)).iteritems()][:100]
 
     seeds = read_newline_file(all_args.seeds)
 
